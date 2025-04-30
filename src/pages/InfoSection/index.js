a725b386c4730396a7f36fbda43fd54eaf399894
AlenAbrahamyan <alenabrahamyan0@gmail.com>
2025-04-30 11:42:18 +0400

    first commit

--- src/pages/InfoSection/index.js
+++ src/pages/InfoSection/index.js
@@ -3,7 +3,7 @@ import {store} from "../../store";
 
 export const InfoSection = () => {
     return <div className={'info-section'}>
-        <img src={'./info-section.png'}/>
+        <img src={'./info-section.png'} alt={'info'}/>
         <div className={'menu-item'} style={{fontSize: '50px'}} onClick={() => store.setPageName(pageNames.MEIN_MENU)}>BACK</div>
     </div>
 }
